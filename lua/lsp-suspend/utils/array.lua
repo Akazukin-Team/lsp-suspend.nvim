b23ece0b433941474608eb777675559baa706dac
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-28 15:33:04 +0900

    update: update params and check nils

--- lua/lsp-suspend/utils/array.lua
+++ lua/lsp-suspend/utils/array.lua
@@ -1,17 +1,21 @@
 local M = {}
 
 --- get keys from table
----@param tbl table
+---@param t table
 ---@return table
-function M.get_keys(tbl)
+function M.get_keys(t)
     local keys = {}
-    for k, _ in pairs(tbl) do
+    for k, _ in pairs(t) do
         table.insert(keys, k)
     end
     return keys
 end
 
 function M.has_value(t, val)
+    if not t then
+        return false
+    end
+
     for _, value in ipairs(t) do
         if value == val then
             return true
@@ -21,6 +25,10 @@ function M.has_value(t, val)
 end
 
 function M.removeByValue(t, v)
+    if not t then
+        return false
+    end
+
     local res = false
     for i = #t, 1, -1 do
         if t[i] == v then
@@ -32,12 +40,24 @@ function M.removeByValue(t, v)
 end
 
 function M.copy(t)
+    if not t then
+        return {}
+    end
+
     return table.move(t, 1, #t, 1, {})
 end
 
+--- Append elements of one table to another
+---@param t table the destination table to append to
+---@param t2 table the source table to append from
+---@return table the destination table after appending
 function M.append(t, t2)
-    local new = copy(t)
-    table.move(t2, new, #t2, #new + 1, new)
+    if not t or not t2 then
+        return M.copy(t or t2 or {})
+    end
+
+    local new = M.copy(t)
+    table.move(t2, 1, #t2, #new + 1, new)
     return new
 end
 
